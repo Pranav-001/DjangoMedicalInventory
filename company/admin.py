a16c6e91544144ab9e3be5517f839e2f90e6f32b
pranav <pranav@suraasa.com>
2022-08-18 02:20:36 +0530

    final

--- company/admin.py
+++ company/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from django.contrib.auth.models import Group
 
 from .models import Company
 
+admin.site.unregister(Group)
 
 # Register your models here.
 @admin.register(Company)
